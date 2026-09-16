15569cfdf69268003f2f2e7400b6c97d656c698c
RezzaDevlopKai <aripribadis10@gmail.com>
2026-09-17 01:19:46 +0700

    feat(trading): expose trading core public API

---
+++ packages/trading/src/index.ts
@@ -0,0 +1,7 @@
+export {
+  calculateFee,
+  matchLimitOrder,
+  normalizeDecimal,
+  validateLimitOrder
+} from "./engine.js";
+export type { LimitOrder, MatchResult, OrderSide, OrderStatus, Trade } from "./engine.js";
