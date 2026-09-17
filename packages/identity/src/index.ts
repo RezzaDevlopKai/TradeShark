af71a280b370fc0154b0e1ecffaf7a99e0790842
RezzaDevlopKai <aripribadis10@gmail.com>
2026-09-18 00:44:28 +0700

    feat(identity): export identity service

--- packages/identity/src/index.ts
+++ packages/identity/src/index.ts
@@ -1,3 +1,13 @@
 export { hashPassword, verifyPassword } from "./password.js";
 export type { PasswordHash } from "./password.js";
 export { createSessionToken, hashSessionToken, SESSION_TOKEN_BYTES } from "./session.js";
+export {
+  IdentityError,
+  IdentityService,
+  SESSION_TTL_MS
+} from "./service.js";
+export type {
+  IdentityServiceOptions,
+  PublicUser,
+  SessionResult
+} from "./service.js";
