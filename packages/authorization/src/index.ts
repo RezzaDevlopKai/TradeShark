31f4cea1c73b5c79373ffda690fbef27190bddaf
RezzaDevlopKai <aripribadis10@gmail.com>
2026-09-18 01:59:38 +0700

    feat(authz): expose authorization policy primitives

---
+++ packages/authorization/src/index.ts
@@ -0,0 +1,14 @@
+export {
+  ACCOUNT_ROLES,
+  DEFAULT_USER_PERMISSIONS,
+  PERMISSIONS,
+  ROLE_PERMISSIONS,
+  authorize,
+  hasPermission
+} from "./policy.js";
+export type {
+  AccountRole,
+  AuthorizationContext,
+  AuthorizationDecision,
+  Permission
+} from "./policy.js";
