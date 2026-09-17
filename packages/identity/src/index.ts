ac4654f51e61ed6614bfe29feab497a0838c98f0
RezzaDevlopKai <aripribadis10@gmail.com>
2026-09-18 00:24:45 +0700

    feat(identity): expose authentication primitives

---
+++ packages/identity/src/index.ts
@@ -0,0 +1,3 @@
+export { hashPassword, verifyPassword } from "./password.js";
+export type { PasswordHash } from "./password.js";
+export { createSessionToken, hashSessionToken, SESSION_TOKEN_BYTES } from "./session.js";
