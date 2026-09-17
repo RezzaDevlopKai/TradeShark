cd665973ac62bed41c53349c0691dfe9c90ce7c6
RezzaDevlopKai <aripribadis10@gmail.com>
2026-09-18 00:43:48 +0700

    feat(identity): include auth tables in database client

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,10 +1,11 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "./schema/index.js";
+import * as identity from "./identity.js";
 
 export function createDatabase(connectionString: string) {
   const pool = new Pool({ connectionString });
-  const db = drizzle(pool, { schema });
+  const db = drizzle(pool, { schema: { ...schema, ...identity } });
 
   return { db, pool };
 }
